cb76922f46df237b4f19302a9867665637e515f1
Roberto <betinhosp2007@gmail.com>
2024-08-30 10:42:53 -0300

    att-site-feira

--- src/pages/home/comps/eventos/index.jsx
+++ src/pages/home/comps/eventos/index.jsx
@@ -1,11 +1,11 @@
 import "./index.scss"
 
-export function Feira(){
+export function Feira(props){
 return(
 <div className="container-feira-profissoes">
 
 <div className="faixa"></div>
-<div className="cabecalho"><h1 className="o-que-tera">Feira das Profissões</h1></div>
+<div className="cabecalho"> <h1 className="o-que-tera">Feira das Profissões</h1></div>
 <div className="carrocel">
     
     <div className="rolagem"><img src="https://s3-alpha-sig.figma.com/img/8df4/1c8b/d0835bb6f985a59f4ae6463ed0d05647?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pGTh8c8syPlGVoo~Yn00gU0PwO2~H7iEde9j6iLwvlotV4BmJ1PM-t6xjSK7jt-d8wBUUQyG3VA7WCapX~ggV6P8wwzPM1OmBpfkyJ62A7EmVx~xM~5VjJ9ClrSC5jwlfRVFnW8GzwA2UJc9WkQGdW7yXqkAqTBdcDMKXbexclogek04qlxBNfjhjO023pJtJ~gMAhPe6IjFtpdY4FHT8OTibuAdWztqsV04wabQjN2LoR2gC4fcVMUMsJqHXkfPPW7UjRh97GKkpHKsjmiA1hv5NUzwFeBwQknegniDf0rpNi6cB7qv-VednN3YBK6lv1goLAHAOmQuWR-SV1ISJw__" alt="" /></div>
